766ae094c30520a916d636cf165a15843c175f7c
Bryan Caballero <90062856+bryacvi@users.noreply.github.com>
2023-04-18 15:09:05 -0700

    ultimos cambios

--- app/src/main/java/com/example/simple_notepads/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/simple_notepads/ui/home/HomeFragment.kt
@@ -1,21 +1,17 @@
 package com.example.simple_notepads.ui.home
 
 import android.R
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.simple_notepads.BlankFragment
-import com.example.simple_notepads.databinding.FragmentBlankBinding
 import com.example.simple_notepads.databinding.FragmentHomeBinding
-import kotlinx.android.synthetic.main.content_main.nav_host_fragment_content_main
 
 
 class HomeFragment : Fragment() {
@@ -37,9 +33,10 @@ class HomeFragment : Fragment() {
         adapter = MovieAdapter()
         binding.rvItemList.adapter = adapter
 
-
         binding.fab.setOnClickListener {
-            fragmentManager.beginTransaction().replace(R.id.container, MyFragment.newInstance(arg1, arg2)).commit();
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(id, BlankFragment())
+            transaction.commit()
         }
 
         val linearLayoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
