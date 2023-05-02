406929ee5d432312f0554cb8eb7e203d0683c76b
Bryan Caballero <90062856+bryacvi@users.noreply.github.com>
2023-05-01 18:58:07 -0700

    Update NewNoteFragment.kt

--- app/src/main/java/com/example/simple_notepads/ui/NoteManagement/NewNoteFragment.kt
+++ app/src/main/java/com/example/simple_notepads/ui/NoteManagement/NewNoteFragment.kt
@@ -1,11 +1,16 @@
 package com.example.simple_notepads.ui.noteManagement
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.example.simple_notepads.R
 
 class NewNoteFragment : Fragment() {
@@ -23,6 +28,24 @@ class NewNoteFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(NewNoteViewModel::class.java)
         // TODO: Use the ViewModel
+        val editWordView = view?.findViewById<EditText>(R.id.edit_word)
+        val button = view?.findViewById<Button>(R.id.button_save)
+
+        button?.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView?.text)) {
+                activity?.setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView?.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                activity?.setResult(Activity.RESULT_OK, replyIntent)
+            }
+            activity?.finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 
 }
