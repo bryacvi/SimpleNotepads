7bf7951ca9999144513fa92cd2d14d139fca2103
Rachelle Reyes <reyes.rachelle@uabc.edu.mx>
2023-05-04 09:45:35 -0700

    Modified code for saving a word in a fragment

--- app/src/main/java/com/example/simple_notepads/ui/NoteManagement/NewNoteFragment.kt
+++ app/src/main/java/com/example/simple_notepads/ui/NoteManagement/NewNoteFragment.kt
@@ -1,25 +1,27 @@
 package com.example.simple_notepads.ui.noteManagement
 
-import android.app.Activity
-import android.content.Intent
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.text.TextUtils
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.simple_notepads.R
-import com.example.simple_notepads.ui.home.MovieAdapter
+import com.example.simple_notepads.examples.word.Word
+import com.example.simple_notepads.examples.word.WordViewModel
+import com.example.simple_notepads.WordsApplication
 
 class NewNoteFragment : Fragment() {
-
-    lateinit var adapter: WordListAdapter
     private lateinit var viewModel: NewNoteViewModel
+    private val wordViewModel: WordViewModel by viewModels {
+        WordViewModelFactory((activity?.application as WordsApplication).repository)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,26 +33,29 @@ class NewNoteFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(NewNoteViewModel::class.java)
-        adapter = WordListAdapter()
+
+        // We can use Binding instead of looking for views like this
         val editWordView = view?.findViewById<EditText>(R.id.edit_word)
         val button = view?.findViewById<Button>(R.id.button_save)
 
         button?.setOnClickListener {
-            val replyIntent = Intent()
+            Log.i(TAG, "Save button pressed")
+
             if (TextUtils.isEmpty(editWordView?.text)) {
-                activity?.setResult(Activity.RESULT_CANCELED, replyIntent)
+                // Add a message to say field is empty, with SnackBar or Toast
             } else {
                 val word = editWordView?.text.toString()
-                replyIntent.putExtra(EXTRA_REPLY, word)
-                activity?.setResult(Activity.RESULT_OK, replyIntent)
+                Log.i(TAG, word)
+                wordViewModel.insert(Word(word))
+                // Use a SnackBar to add success message for saving
+                // Navigate back to Home.
+                findNavController().navigateUp()
             }
-            //App crashes after pressing save
-            //activity?.finish()
         }
     }
 
     companion object {
-        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+        private const val TAG = "NewNoteFragment"
     }
 
 }
